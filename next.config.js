29fd9a335f5462079c20fad4b0e1125349f772b6
missionctrlbiz <teewhy_007@yahoo.com>
2024-05-30 13:51:26 +0100

    final commit to vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+const basePath = process.env.NODE_ENV === 'production' ? '/dashboard' : '';
+
+module.exports = {
+  basePath,
+};
